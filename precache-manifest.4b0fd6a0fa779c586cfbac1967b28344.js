9328a53cc6e5249b34a5aa19b63200cfd78ddbda
bradleyham <bradleyjamesham@gmail.com>
2020-08-22 11:55:04 +1200

    Updates

---
+++ precache-manifest.4b0fd6a0fa779c586cfbac1967b28344.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fd651fb4c2d3b6aaf88a9c2f2ef40f",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "f8ed6e6652d9780175e5",
+    "url": "/restaurant/static/css/main.030d797a.chunk.css"
+  },
+  {
+    "revision": "ae6f7d2f538d5315310c",
+    "url": "/restaurant/static/js/2.fa6edb91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/restaurant/static/js/2.fa6edb91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ed6e6652d9780175e5",
+    "url": "/restaurant/static/js/main.6d75614b.chunk.js"
+  },
+  {
+    "revision": "2d695fe8ffcd6ab8a602",
+    "url": "/restaurant/static/js/runtime-main.ff89f540.js"
+  }
+]);
